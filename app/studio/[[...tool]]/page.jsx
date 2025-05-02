8d80eb5bd67ce9b4850efffc1437b9fa9e5faf22
Joshua. C <76528955+jemcap@users.noreply.github.com>
2025-05-02 16:15:54 +0100

    Route to homepage when accessing studio in production

--- app/studio/[[...tool]]/page.jsx
+++ app/studio/[[...tool]]/page.jsx
@@ -7,13 +7,17 @@
  * https://github.com/sanity-io/next-sanity
  */
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
+import { NextStudio } from "next-sanity/studio";
+import config from "../../../sanity.config";
+import { redirect } from "next/navigation";
 
-export const dynamic = 'force-static'
+export const dynamic = "force-static";
 
-export { metadata, viewport } from 'next-sanity/studio'
+export { metadata, viewport } from "next-sanity/studio";
+if (process.env.NODE_ENV === "production") {
+  redirect("/");
+}
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <NextStudio config={config} />;
 }
