fb1efde97b5ddcf294ee7516eb2b960b128bd12e
Joshua. C <76528955+jemcap@users.noreply.github.com>
2025-05-14 10:29:40 +0100

    Improved Navbar component

--- components/navbar/navbar.component.tsx
+++ components/navbar/navbar.component.tsx
@@ -2,23 +2,13 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { NavigationList } from "./navigation-menu.component";
+import Navigation from "./navigation.component";
 
 const Navbar: React.FC = async () => {
   return (
     <header className="px-5 fixed py-3 w-full font-work-sans bg-black/80 backdrop-blur-sm z-50">
       <nav className="flex justify-between items-center align-element ">
-        <Link href="/">
-          <Image
-            src="/shuacapstudio-logo.png"
-            alt="logo"
-            width={75}
-            height={75}
-          />
-        </Link>
-
-        <div className="flex items-center gap-5 text-white">
-          <NavigationList />
-        </div>
+        <Navigation />
       </nav>
     </header>
   );
